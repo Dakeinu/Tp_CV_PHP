417693fab5ad03b8bb48964a72b62ca1dd5d95a6
Dakeinu <61381967+Dakeinu@users.noreply.github.com>
2020-02-23 14:48:47 +0100

    Add files via upload

---
+++ admin/partials/createpage.php
@@ -0,0 +1,12 @@
+<form method="post">
+<div class="form-group">
+    <label for="exampleFormControlTextarea1">Votre Message :</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="commentaire"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Votre Message :</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="commentaire"></textarea>
+  </div>
+
+  <input type="submit" name="valider" value="Envoyer"/>
+</form>
---
+++ admin/partials/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <a href="https://www.linkedin.com/in/r%C3%A9mi-falcati-7b2556194/" class="fa fa-linkedin"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    <a href="https://www.facebook.com/remi.falcati" class="fa fa-facebook"></a>
+</footer>
